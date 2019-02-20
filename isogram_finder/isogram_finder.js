f8b392f5769e368bf673a6fbd3f58f2c3013a43d
Anthony Starke <starke@gmail.com>
2019-02-20 19:44:05 +0000

    finished isogram

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
+  this.word = word.toLowerCase().split('');
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  const result = this.word.reduce((total,letter) => (!total.includes(letter) ? total+=letter: ""))
+  return (result.length === this.word.length ? true : false)
 }
 
 module.exports = IsogramFinder;
